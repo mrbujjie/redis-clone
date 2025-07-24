7f06a8e59b7e75fd4ec8009a507bbd73c561c41b
mrbujjie <srikanthrp51@gmail.com>
2025-07-24 08:06:20 +0000

    "The code was slightly modified for debugging help , there was no changes in single server handling"

--- server/server.cpp
+++ server/server.cpp
@@ -1,32 +1,33 @@
-#include <assert.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include <errno.h>
+#include <cassert>
+#include <cstdint>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <cerrno>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/ip.h>
 
+using namespace std;
+
+const size_t k_max_msg = 4096;
 
 static void msg(const char *msg) {
-    fprintf(stderr, "%s\n", msg);
+    cerr << msg << endl;
 }
 
 static void die(const char *msg) {
     int err = errno;
-    fprintf(stderr, "[%d] %s\n", err, msg);
-    abort();
+    cerr << "[" << err << "] " << msg << endl;
+    exit(1);
 }
-
-const size_t k_max_msg = 4096;
 
 static int32_t read_full(int fd, char *buf, size_t n) {
     while (n > 0) {
         ssize_t rv = read(fd, buf, n);
         if (rv <= 0) {
-            return -1;  // error, or unexpected EOF
+            return -1;  // error or EOF
         }
         assert((size_t)rv <= n);
         n -= (size_t)rv;
@@ -49,7 +50,6 @@ static int32_t write_all(int fd, const char *buf, size_t n) {
 }
 
 static int32_t one_request(int connfd) {
-    // 4 bytes header
     char rbuf[4 + k_max_msg];
     errno = 0;
     int32_t err = read_full(connfd, rbuf, 4);
@@ -59,23 +59,21 @@ static int32_t one_request(int connfd) {
     }
 
     uint32_t len = 0;
-    memcpy(&len, rbuf, 4);  // assume little endian
+    memcpy(&len, rbuf, 4);  // assuming little-endian
     if (len > k_max_msg) {
-        msg("too long");
+        msg("message too long");
         return -1;
     }
 
-    // request body
     err = read_full(connfd, &rbuf[4], len);
     if (err) {
-        msg("read() error");
+        msg("read() error while reading body");
         return err;
     }
 
-    // do something
-    fprintf(stderr, "client says: %.*s\n", len, &rbuf[4]);
+    cerr << "client says: " << string(&rbuf[4], len) << endl;
 
-    // reply using the same protocol
+    // Send reply
     const char reply[] = "world";
     char wbuf[4 + sizeof(reply)];
     len = (uint32_t)strlen(reply);
@@ -85,49 +83,56 @@ static int32_t one_request(int connfd) {
 }
 
 int main() {
+    cerr << "Starting server..." << endl;
+
     int fd = socket(AF_INET, SOCK_STREAM, 0);
     if (fd < 0) {
         die("socket()");
     }
+    cerr << "Socket created." << endl;
 
-    // this is needed for most server applications
     int val = 1;
     setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
 
-    // bind
     struct sockaddr_in addr = {};
     addr.sin_family = AF_INET;
-    addr.sin_port = ntohs(1234);
-    addr.sin_addr.s_addr = ntohl(0);    // wildcard address 0.0.0.0
-    int rv = bind(fd, (const sockaddr *)&addr, sizeof(addr));
-    if (rv) {
+    addr.sin_port = htons(1234);
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    int rv = bind(fd, (const struct sockaddr *)&addr, sizeof(addr));
+    if (rv != 0) {
         die("bind()");
     }
+    cerr << "Bind successful." << endl;
 
-    // listen
     rv = listen(fd, SOMAXCONN);
-    if (rv) {
+    if (rv != 0) {
         die("listen()");
     }
+    cerr << "Listening on port 1234..." << endl;
 
     while (true) {
-        // accept
         struct sockaddr_in client_addr = {};
         socklen_t addrlen = sizeof(client_addr);
+        cerr << "Waiting for connection..." << endl;
         int connfd = accept(fd, (struct sockaddr *)&client_addr, &addrlen);
         if (connfd < 0) {
-            continue;   // error
+            msg("accept() failed, continuing...");
+            continue;
         }
+        cerr << "Client connected." << endl;
 
         while (true) {
-            // here the server only serves one client connection at once
             int32_t err = one_request(connfd);
             if (err) {
+                cerr << "Closing connection." << endl;
                 break;
             }
         }
+
         close(connfd);
     }
 
     return 0;
 }
+
